9990152f817e04dac7c864e953442bb1419ce7ba
alegjs044 <alegjs044@gmail.com>
2024-05-26 03:28:46 -0600

    add files

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,25 +1,30 @@
 // src/components/Navbar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../imagenes/logoESCOM2x.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={logo} alt="Logo" style={{ height: '80px', marginRight: '90px' }} />
+        <h2> Trabajos de titulación</h2> {/* Puedes añadir un nombre o título */}
+      </div>
       <ul>
         <li>
-          <Link to="/">Inicio</Link>
+          <Link to="/" style={{ color: 'beige' }}>Inicio</Link>
         </li>
         <li>
-          <Link to="/Library">Biblioteca</Link>
+          <Link to="/Library" style={{ color: 'beige' }}>Biblioteca</Link>
         </li>
         <li>
-          <Link to="/Search">Búsquedas</Link>
+          <Link to="/Search" style={{ color: 'beige' }}>Búsquedas</Link>
         </li>
         <li>
-          <Link to="/Profile">Perfil</Link>
+          <Link to="/Profile" style={{ color: 'beige' }}>Perfil</Link>
         </li>
         <li>
-          <Link to="/Login">Login</Link>
+          <Link to="/Login" style={{ color: 'beige' }}>Login</Link>
         </li>
       </ul>
     </nav>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import '../estilos/estilos.css';
-import imagen from '../imagenes/5528913.jpg'; // Asegúrate de usar la extensión correcta de la imagen
+import imagen2 from '../imagenes/library-7720589_1280.jpg'; // Asegúrate de usar la extensión correcta de la imagen
 
 const Home = () => {
   return (
     <div className="home">
-      <div className="grid-container">
-        <div className="item1" style={{ backgroundImage: `url(${imagen})` }}>
-          <div className="overlay">
-            <h2>Texto para la imagen 1</h2>
-          </div>
+      <div className="item1">
+        <h1>Sobre nosotros</h1>
+        <p>Este proyecto sirve como biblioteca para los proyectos de Titulación, 
+          donde se podra buscar y acceder por medio de un perfil </p>
+          <div class="fb-page" data-href="https://www.facebook.com/Catt.ESCOM.Oficial" data-tabs="timeline" data-width="500" data-height="300" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/Catt.ESCOM.Oficial" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Catt.ESCOM.Oficial">CATT ESCOM</a></blockquote></div>
+      </div>
+      {/* Componente más pequeño al lado */}
+      <div className="item2" style={{ backgroundImage: `url(${imagen2})` }}>
+        <div className="overlay">
         </div>
-        <div className="item2" style={{ backgroundImage: `url(${imagen})` }}>
-          <div className="overlay">
-            <h2>Texto para la imagen 2</h2>
-          </div>
-        </div>
-        {/* Agrega más divisiones aquí si es necesario */}
       </div>
     </div>
   );
